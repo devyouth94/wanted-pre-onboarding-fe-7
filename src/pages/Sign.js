5945e39446b3d30129a723b8e4601fb27d9c19d5
devyouth94 <devyouth94@gmail.com>
2022-10-10 00:45:12 +0900

    로그인 회원가입 구현

---
+++ src/pages/Sign.js
@@ -0,0 +1,158 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import instance from "../app/instance";
+
+import styled from "styled-components";
+import { StContentsWrap, StInput, StTitle } from "../shared/GlobalStyle";
+
+const Sign = () => {
+  const navigate = useNavigate();
+
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [checkEmail, setCheckEmail] = useState(false);
+  const [checkPassword, setCheckPassword] = useState(false);
+
+  const isCorrectEmail = (value) => {
+    return /\w+@\w/.test(value);
+  };
+
+  const isCorrectPassword = (value) => {
+    return value.length >= 8;
+  };
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+    if (name === "email") {
+      setCheckEmail(isCorrectEmail(value));
+    } else {
+      setCheckPassword(isCorrectPassword(value));
+    }
+  };
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      if (isSignUp) {
+        await instance.post("/auth/signup", user);
+        setIsSignUp(false);
+        alert("회원가입 완료!");
+      } else {
+        const { data } = await instance.post("/auth/signin", user);
+        localStorage.setItem("token", data.access_token);
+        navigate("/todo");
+      }
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      navigate("/todo");
+    }
+  }, [navigate]);
+
+  return (
+    <StContentsWrap>
+      <StTitle>{isSignUp ? "회원가입" : "로그인"}</StTitle>
+
+      <StForm onSubmit={onSubmitHandler}>
+        <StArticle>
+          <StLabel>아이디</StLabel>
+          <StInput
+            type="text"
+            name="email"
+            value={user.email}
+            onChange={onChangeHandler}
+            placeholder="이메일을 입력해주세요"
+          />
+          {!checkEmail && <StHelper>올바른 형식이 아닙니다</StHelper>}
+        </StArticle>
+
+        <StArticle>
+          <StLabel>비밀번호</StLabel>
+          <StInput
+            type="password"
+            name="password"
+            value={user.password}
+            onChange={onChangeHandler}
+            placeholder="비밀번호를 입력해주세요"
+          />
+          {!checkPassword && <StHelper>비밀번호는 8자 이상입니다</StHelper>}
+        </StArticle>
+
+        <StButton disabled={checkEmail && checkPassword ? false : true}>{isSignUp ? "회원가입" : "로그인"}</StButton>
+      </StForm>
+
+      <StNavi onClick={() => setIsSignUp((prev) => !prev)}>{isSignUp ? "로그인" : "회원가입"} 하러가기</StNavi>
+    </StContentsWrap>
+  );
+};
+
+export default Sign;
+
+const StForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  width: 100%;
+`;
+
+const StArticle = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StLabel = styled.div`
+  margin-bottom: 5px;
+
+  font-weight: 500;
+`;
+
+const StHelper = styled.span`
+  margin-top: 7px;
+  margin-left: 5px;
+
+  font-size: 12px;
+  color: #fe9e2d;
+`;
+
+const StButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 40px;
+  background-color: transparent;
+
+  border: 1.2px solid #fe9e2d;
+  border-radius: 10px;
+
+  color: #fe9e2d;
+  font-weight: 500;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: #fe9e2d;
+    color: #fff;
+  }
+
+  &:disabled {
+    background-color: #ddd;
+    border: none;
+    color: #bbb;
+  }
+`;
+
+const StNavi = styled.span`
+  margin-top: 30px;
+
+  font-size: 14px;
+  color: #aaa;
+`;
