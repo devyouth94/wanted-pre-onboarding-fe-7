4b8b6198493d5bf9e6d190bc3c1766520637519a
devyouth94 <devyouth94@gmail.com>
2022-10-10 00:44:44 +0900

    router 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Sign from "./pages/Sign";
+import TodoList from "./pages/TodoList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Sign />} />
+      <Route path="/todo" element={<TodoList />} />
+    </Routes>
+  );
 };
 
 export default App;
